321d40db36810a3bcb2fd786027d5e2384909116
Luanefernandes <65264021+Luanefernandes@users.noreply.github.com>
2020-08-06 10:36:08 -0300

    POO-2

---
+++ POO_2/Carro_2.java
@@ -0,0 +1,25 @@
+package POO_2;
+
+public class Carro_2 {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	public Carro_2(String marca, String modelo, int numPassageiros,
+			double capCombustivel, double consumoCombustivel) {
+		super();
+		this.marca = marca;
+		this.modelo = modelo;
+		this.numPassageiros = numPassageiros;
+		this.capCombustivel = capCombustivel;
+		this.consumoCombustivel = consumoCombustivel;
+	}
+
+	public Carro_2() {
+		
+	}
+
+}
